3ef723d220192f3dd2849fc30203351f60f2d300
Lens0021 <lorentz0021@gmail.com>
2022-04-19 19:31:35 +0900

    Add wgUnifiedExtensionForFemiwikiModifyEmailTheme (#91)
    Migrated from Femiwik skin.
    https://github.com/femiwiki/FemiwikiSkin/tree/c7c4d2f9933584100ce5db5a035691d5a185e050

--- includes/HookHandlers/Main.php
+++ includes/HookHandlers/Main.php
@@ -3,6 +3,7 @@
 namespace MediaWiki\Extension\UnifiedExtensionForFemiwiki\HookHandlers;
 
 use Config;
+use ExtensionRegistry;
 use Html;
 use RequestContext;
 use Skin;
@@ -14,6 +15,7 @@ class Main implements
 	\MediaWiki\Hook\LinkerMakeExternalLinkHook,
 	\MediaWiki\Hook\SidebarBeforeOutputHook,
 	\MediaWiki\Hook\SkinAddFooterLinksHook,
+	\MediaWiki\Hook\UserMailerTransformContentHook,
 	\MediaWiki\Linker\Hook\HtmlPageLinkRendererBeginHook
 	{
 
@@ -155,5 +157,43 @@ class Main implements
 		}
 
 		return false;
+	}
+
+	/**
+	 * Echo(REL1_31)'s content values
+	 * @See https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/Echo/+/REL1_31/includes/formatters/EchoHtmlEmailFormatter.php
+	 */
+	// phpcs:ignore Generic.Files.LineLength.TooLong
+	private const PRIMARY_LINK_STYLE = 'cursor:pointer; text-align:center; text-decoration:none; padding:.45em 0.6em .45em; color:#FFF; background:#36C; font-family: Arial, Helvetica, sans-serif;font-size: 13px;';
+	private const FOOTER_PREFERENCE_LINK_STYLE = 'text-decoration: none; color: #36C;';
+
+	/**
+	 * Styles for Femiwiki
+	 */
+	// phpcs:ignore Generic.Files.LineLength.TooLong
+	private const FEMIWIKI_PRIMARY_LINK_STYLE = 'cursor:pointer; text-align:center; text-decoration:none; padding:.45em 0.6em .45em; color:#FFF; background:#aca7e2; font-family: Arial, Helvetica, sans-serif;font-size: 13px;';
+	private const FEMIWIKI_FOOTER_PREFERENCE_LINK_STYLE = 'text-decoration: none; color: #5144a3;';
+
+	/**
+	 * Modifying HTML mails sent from Echo.
+	 * @inheritDoc
+	 */
+	public function onUserMailerTransformContent( $to, $from, &$body, &$error ) {
+		if (
+			!$this->config->get( 'UnifiedExtensionForFemiwikiModifyEmailTheme' ) ||
+			!ExtensionRegistry::getInstance()->isLoaded( 'Echo' ) ||
+			!is_array( $body ) ||
+			!isset( $body['html'] )
+		) {
+			return;
+		}
+
+		$body['html'] = str_replace(
+			[ self::PRIMARY_LINK_STYLE,
+				self::FOOTER_PREFERENCE_LINK_STYLE ],
+			[ self::FEMIWIKI_PRIMARY_LINK_STYLE,
+				self::FEMIWIKI_FOOTER_PREFERENCE_LINK_STYLE ],
+			$body['html']
+		);
 	}
 }
